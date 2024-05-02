9d43b778c4397834937105e81f810cdfa0eadf55
Nicole Farinello <nicole_mattos@live.com>
2024-05-02 17:32:36 +0100

    update name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather App</h1>
+        <h1>Weather React App</h1>
         <Weather />
       </header>
     </div>
